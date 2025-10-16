1b4fb55365fdf22ec3312de4fa39c8c0500fd7e9
Dmitri Faizullin <faizullin-dmitri@mail.ru>
2025-10-16 15:34:27 +0500

    added apps and models

---
+++ test_project_drf/products/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class Category(models.Model):
+    """Модель для категорий товаров."""
+    name = models.CharField('Название', max_length=30)
+    slug = models.SlugField('Слаг', max_length=30, unique=True)
+    image = models.ImageField(
+        upload_to='categories/images/',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    """Модель для подкатегорий товаров."""
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='subcategories',
+        verbose_name='Подкатегория'
+    )
+    name = models.CharField('Название', max_length=30)
+    slug = models.SlugField('Слаг', max_length=30, unique=True)
+    image = models.ImageField(
+        'Изображение',
+        upload_to='subcategories/images/',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    """Модель для товаров."""
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name='Товар'
+    )
+    name = models.CharField('Название', max_length=100)
+    slug = models.SlugField('Слаг', max_length=100, unique=True)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    image_small = models.ImageField(
+        'Изображение (малое)',
+        upload_to='products/images/small/',
+        null=True,
+        blank=True
+    )
+    image_medium = models.ImageField(
+        'Изображение (среднее)',
+        upload_to='products/images/medium/',
+        null=True,
+        blank=True
+    )
+    image_large = models.ImageField(
+        'Изображение (большое)',
+        upload_to='products/images/large/',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
---
+++ test_project_drf/users/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.core.validators import MinValueValidator
+from products.models import Products
+
+
+class CustomUser(AbstractUser):
+    cart_products = models.ManyToManyField(
+        Products,
+        through='Cart',
+        through_fields=('user', 'product'),
+        related_name='in_user_cart',
+        verbose_name='Товары в корзине',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='cart_items',
+        verbose_name='Пользователь'
+    )
+    product = models.ForeignKey(
+        Products,
+        on_delete=models.CASCADE,
+        related_name='cart_entries',
+        verbose_name='Товар'
+    )
+    quantity = models.PositiveIntegerField(
+        'Количество',
+        default=1,
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.product.name} ({self.quantity})'
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
